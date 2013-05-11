3054754f16a568198ee1759c8c6488502650aa9a
Alex Cappiello <acappiel@andrew.cmu.edu>
2013-05-11 01:49:51 -0400

    Collected perf data and code to generate plots.
    Signed-off-by: Alex Cappiello <acappiel@andrew.cmu.edu>

---
+++ plots.m
@@ -0,0 +1,23 @@
+%%
+nparticle = [1024 2048 4096 6144 8192 12288 16384 16384];
+group = [16 32 64 128 256 512 1024];
+bbox = [100 100 100 100 100 100 100 200];
+
+%%
+for i = 1:length(nparticle)
+    figure(i);
+    hold on;
+    title([int2str(nparticle(i)) ' Particles, ' int2str(bbox(i)) ' box']);
+    xlabel('Group Size');
+    ylabel('FPS');
+    plot(group, fn(i,:), '--ob', 'MarkerEdgeColor', 'black', ...
+        'MarkerFaceColor', 'b');
+    plot(group, fnc(i,:), '--og', 'MarkerEdgeColor', 'black', ...
+        'MarkerFaceColor', 'g');
+    plot(group, ft(i,:), '--or', 'MarkerEdgeColor', 'black', ...
+        'MarkerFaceColor', 'r');
+    plot(group, ftc(i,:), '--oy', 'MarkerEdgeColor', 'black', ...
+        'MarkerFaceColor', 'y');
+    legend('force\_naive', 'force\_naive\_clip', 'force\_tile', ...
+        'force\_tile\_clip', 'Location', 'SouthOutside');
+end
